12f75fe5e5a6cb6a3419de6f381c9fffc92f3e32
braveheart-star <mouse1996115@gmail.com>
2021-03-31 18:23:07 +0800

    format app module & change prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
