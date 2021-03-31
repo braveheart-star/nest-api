594e604ad8e04dbed84b7d85b910323967cddaaf
braveheart-star <mouse1996115@gmail.com>
2021-03-31 18:10:47 +0800

    generate moule & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
