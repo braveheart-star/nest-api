015ed4d140ffe35b829623dea59d90b00796bc50
braveheart-star <mouse1996115@gmail.com>
2021-03-31 17:05:47 +0800

    create module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
