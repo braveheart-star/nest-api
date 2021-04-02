e09cbf4c56ae9b172206c55c3a2145fa7f156984
braveheart-star <mouse1996115@gmail.com>
2021-04-03 06:07:00 +0800

    add circular dependency(forwardRef) & exports UserService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthService } from "./auth.service";
 import { RolesGuard } from "./guards/roles.guard";
 import { JwtAuthGuard } from "./guards/jwt-guard";
 import { JwtStrategy } from "./guards/jwt-strategy";
+import { UserModule } from "../user/user.module";
 
 @Module({
   imports: [
+    forwardRef(() => UserModule),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,11 +1,21 @@
-import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  forwardRef,
+} from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { UserService } from "../../user/user.service";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector, private userService: UserService) {}
+  constructor(
+    private reflector: Reflector,
+    @Inject(forwardRef(() => UserService))
+    private userService: UserService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
