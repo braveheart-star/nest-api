8f0300a4f3c9a24448ed53cacbffcc53eda20db6
braveheart-star <mouse1996115@gmail.com>
2021-04-11 05:44:18 +0800

    add blog module & implement OneToMany, ManyToOne

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
+import { BlogModule } from "./blog/blog.module";
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,
     AuthModule,
+    BlogModule,
   ],
 })
 export class AppModule {}
